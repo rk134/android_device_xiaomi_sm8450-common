c5ac67628a1172dcadb3238552f80b8611b1b054
rahul <rahul@aospa.co>
2024-11-02 12:09:01 +0000

    sm8450-common: parts: Migrate to collapsingtoolbar.R

--- parts/src/co/aospa/xiaomiparts/gestures/FpDoubleTapActivity.java
+++ parts/src/co/aospa/xiaomiparts/gestures/FpDoubleTapActivity.java
@@ -19,7 +19,7 @@ package co.aospa.xiaomiparts.gestures;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class FpDoubleTapActivity extends CollapsingToolbarBaseActivity {
 
